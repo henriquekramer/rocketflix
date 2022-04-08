ce197afd7dd6343e2099a8cddd540b11f43acf33
Henrique <henriquemkramer@gmail.com>
2022-04-07 22:17:34 -0300

    Ajuste bug sinopse filmes, inclusão nota média

--- main.js
+++ main.js
@@ -6,18 +6,26 @@ shuffleButton.addEventListener('click', getMovie)
 function getMovie() {
   let movieId = Math.floor(Math.random() * 1000 + 1)
   const container = document.querySelector('.container')
-  const url = `${BASE_URL}/${movieId}?api_key=${API_KEY}&${language}`
+  const url = `${BASE_URL}${movieId}?api_key=${API_KEY}&${language}`
   
   axios.get(url)
     .then (response => {
       const data = response.data
-      const movieTitle = data.title
-      const movieOverview = data.overview
-      const poster = `${IMG_URL}`+ data.poster_path
+      let movieTitle = data.title
+      let movieOverview
+      let poster = `${IMG_URL}`+ data.poster_path
+      let rate = data.vote_average
+
+      if(data.overview == ''){
+        movieOverview = 'Desculpe. Não temos uma sinopse em Português deste filme.'
+      } else {
+        movieOverview = data.overview
+      }
 
       container.innerHTML = `
         <div class="movie-poster">
           <img src="${poster}" alt="movie-poster" id="poster">
+          <p>Nota média: ${rate}</p>
         </div>
         <div class="movie-infos">
           <h3 id="titleMovie">${movieTitle}</h3>
@@ -26,7 +34,6 @@ function getMovie() {
       `
     })
     .catch (() =>{
-
       container.innerHTML = `
         <div class="movie-poster">
           <img src="./assets/coding.png" alt="movie-poster" id="poster">
